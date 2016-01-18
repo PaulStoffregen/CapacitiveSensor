2faac421e1540d40bcfb3933d46c93b4fd6c01d7
PaulStoffregen <paul@pjrc.com>
2016-01-18 04:46:01 -0800

    Add MIT license, based on email from Paul Badger

--- CapacitiveSensor.h
+++ CapacitiveSensor.h
@@ -1,12 +1,29 @@
 /*
-  CapacitiveSense.h v.04 - Capacitive Sensing Library for 'duino / Wiring
-  https://github.com/PaulStoffregen/CapacitiveSensor
-  http://www.pjrc.com/teensy/td_libs_CapacitiveSensor.html
-  http://playground.arduino.cc/Main/CapacitiveSensor
-  Copyright (c) 2008 Paul Bagder  All rights reserved.
-  Version 05 by Paul Stoffregen - Support non-AVR board: Teensy 3.x, Arduino Due
-  Version 04 by Paul Stoffregen - Arduino 1.0 compatibility, issue 146 fix
-  vim: set ts=4:
+ CapacitiveSense.h - Capacitive Sensing Library for 'duino / Wiring
+ https://github.com/PaulStoffregen/CapacitiveSensor
+ http://www.pjrc.com/teensy/td_libs_CapacitiveSensor.html
+ http://playground.arduino.cc/Main/CapacitiveSensor
+ Copyright (c) 2009 Paul Bagder
+ Updates for other hardare by Paul Stoffregen, 2010-2016
+ vim: set ts=4:
+
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
 */
 
 // ensure this library description is only included once
