8f7e94bc8d3f0008c8c92a117ae98b440eda13dc
Aurelien BOUIN <aurelien.bouin@captina.dev>
2021-12-01 12:41:02 +0100

    Avoid error: call of overloaded 'abs(long unsigned int)' is ambiguous when compiling for stm32

--- CapacitiveSensor.cpp
+++ CapacitiveSensor.cpp
@@ -74,6 +74,16 @@ CapacitiveSensor::CapacitiveSensor(uint8_t sendPin, uint8_t receivePin)
 	lastCal = millis();         // set millis for start
 }
 
+//Avoid error: call of overloaded 'abs(long unsigned int)' is ambiguous when compiling for stm32
+unsigned long safediffabs(unsigned long one, unsigned long two)
+{
+	if(one > two)
+	{
+		return (one - two);
+	}
+	return (two - one);
+}
+
 // Public Methods //////////////////////////////////////////////////////////////
 // Functions available in Wiring sketches, this library, and other libraries
 
@@ -91,7 +101,7 @@ long CapacitiveSensor::capacitiveSensor(uint8_t samples)
 		// only calibrate if time is greater than CS_AutocaL_Millis and total is less than 10% of baseline
 		// this is an attempt to keep from calibrating when the sensor is seeing a "touched" signal
 
-		if ( (millis() - lastCal > CS_AutocaL_Millis) && abs(total  - leastTotal) < (int)(.10 * (float)leastTotal) ) {
+		if ( (millis() - lastCal > CS_AutocaL_Millis) && safediffabs(total, leastTotal) < (int)(.10 * (float)leastTotal) ) {
 
 			// Serial.println();               // debugging
 			// Serial.println("auto-calibrate");
